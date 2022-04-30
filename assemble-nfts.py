5329f3a46744fb37ae367c0e9e8be80d57d6db31
Kenny <kllort@gmail.com>
2022-04-30 00:13:18 -0400

    added weights and added json output

--- assemble-nfts.py
+++ assemble-nfts.py
@@ -1,10 +1,26 @@
 '''Layering and Combining Images'''
-import nftmaker as nft  
-
+import nftmaker as nft
+import datetime
 
 if __name__ == "__main__":
-    collection_size=10
-    
-    nft.createNFT(collection_size)
+
+    collection_size=100
+    json_output_filename = 'output/1json.json'
+    """
+    The List is each folder of traits, in order of how they should be layered.
+    """
+    myList=[
+    'data/lion/robe/',
+    'data/lion/belts/',
+    'data/lion/arms/',
+    'data/lion/emblem/',
+    'data/lion/accessory/',
+    'data/lion/head/',
+    'data/lion/head_headgear/',
+    'data/lion/mouth/',
+    'data/lion/eyes/'
+    ]
+    nft.createNFT(collection_size, myList,json_output_filename)
+   
 
 
