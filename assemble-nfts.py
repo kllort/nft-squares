208f0e17ef331065b996f8d28ea01ba3f6f2a6cc
kllort <62654702+kllort@users.noreply.github.com>
2022-08-29 15:49:10 -0400

    updated nft maker

--- assemble-nfts.py
+++ assemble-nfts.py
@@ -9,20 +9,41 @@ if __name__ == "__main__":
     lists are pngs
     Backgrounds are gifs
     """
-    backgrounds='data/bg/'
-    music='music/'
-    myList=[
-    'data/lion/robe/',
-    'data/lion/belts/',
-    'data/lion/arms/',
-    'data/lion/emblem/',
-    'data/lion/accessory/',
-    'data/lion/head/',
-    'data/lion/head_headgear/',
-    'data/lion/mouth/',
-    'data/lion/eyes/'
+    backgrounds='data/bgs/'
+    music='data/music/'
+    eagle=[
+    'data/eagle/1-skins/',
+    'data/eagle/3-eyes/',
+    'data/eagle/2-beaks/',
+    'data/eagle/4-headgear/',
+    'data/weapons/R/'
     ]
-    nft.createNFT(collection_size, myList,json_output_filename,backgrounds, music)
-   
+
+    panda=[
+    'data/panda/1-skins/',
+    'data/panda/2-eyes/',
+    'data/panda/3-snouts/',
+    'data/panda/4-headgear/',
+    'weapons/combo/'
+    ]
+
+    tiger=[
+    'data/tiger/1-skins/',
+    'data/tiger/2-eyes/',
+    'data/tiger/3-snout/',
+    'data/tiger/4-headgear/',
+    'weapons/combo/'
+    ]
+
+    monke=[
+    'data/monke/1-skins/',
+    'data/monke/2-eyes/',
+    'data/monke/3-mouth/',
+    'data/monke/4-headgear/',
+    'weapons/combo/'
+    ]
+
+    #nft.createNFT(collection_size, eagle,json_output_filename,backgrounds, music)
+    nft.create_my_NFT(collection_size, eagle,json_output_filename,backgrounds)
 
 
--- nftmaker.py
+++ nftmaker.py
@@ -5,6 +5,99 @@ from PIL import ImageDraw
 from PIL import ImageFont
 import json
 from moviepy.editor import AudioFileClip, ImageClip
+import ffmpeg
+
+def create_my_NFT(collection_size, myList,json_output,background):
+    i=1
+    output=[]
+    while i < collection_size+1:
+        myPNG, thisNFT= makePNG(myList,i)
+        paste_png_on_mp4(background,myPNG,i)
+        output.append(thisNFT)
+        i=i+1
+        
+    
+    json_object = json.dumps(output, indent=4)
+    with open(json_output,'w') as f:
+        f.write(json_object)
+        f.close
+
+
+
+
+def createNFT_222(collection_size, myList,json_output,background,music):
+    """each layer should be organized into a folder.
+        then list out the layer folders in z order in the mylist variable
+        then configure and create the art in assemble.nfts.py
+    """
+    i=1
+    output=[]
+    while i < collection_size+1:
+        thisNFT = {}
+
+        ''' add a method to map backgrounds to specific beats'''
+
+        x=0 
+        while x<len(myList):
+            trait = myList[x]
+            llist=os.listdir(trait)
+            lweights = create_weights(llist)
+            choice = pick1(llist,lweights)
+            if x==0:
+                bg=Image.open(trait+choice[0])
+            else:
+                L1=Image.open(trait+choice[0])
+                thisNFT[trait]=choice[0]
+                bg.paste(L1,(0,0),L1)
+            x+=1
+            image_path = 'output/output'+str(i)+'.png'
+            
+            
+            bg_mp4, bg_i=add_mp4(background,)
+        thisNFT[background]=bg_i[0]
+        bg.save(image_path,quality=100)
+        bg = Image.open(image_path)
+        bg = bg.resize((1000,1000))
+        bg = add_text(bg, thisNFT)
+        bg.save(image_path,quality=100)
+        add_static_image_to_audio(image_path, audio_path, output_path)
+        os.remove(image_path)
+        output.append(thisNFT)
+        print(i)
+        i+=1
+    json_object = json.dumps(output, indent=4)
+    with open(json_output,'w') as f:
+        f.write(json_object)
+        f.close
+
+def makePNG(myList,i):
+    """each layer should be organized into a folder.
+        then list out the layer folders in z order in the mylist variable
+        then configure and create the art in assemble.nfts.py
+    """
+    x=0 
+    thisNFT = {}
+
+    trait = myList[x]
+    llist=os.listdir(trait)
+    lweights = create_weights(llist)
+    choice = pick1(llist,lweights)
+    bg=Image.open(trait+choice[0])
+    thisNFT[trait]=choice[0]
+    
+    while x<len(myList):
+        trait = myList[x]
+        llist=os.listdir(trait)
+        lweights = create_weights(llist)
+        choice = pick1(llist,lweights)
+        L1=Image.open(trait+choice[0])
+        thisNFT[trait]=choice[0]
+        bg.paste(L1,(0,0),L1)
+        x+=1
+    image_path = 'output/output'+str(i)+'.png'
+    bg.save(image_path,quality=100)
+    return image_path, thisNFT
+
 
 def createNFT(collection_size, myList,json_output,background,music):
     """each layer should be organized into a folder.
@@ -100,3 +193,19 @@ def add_background(background):
         bg_i = pick1(llist,lweights)
         bg = Image.open(background+bg_i[0]).convert('RGB')
         return  bg, bg_i
+
+def paste_png_on_mp4(background,myPNG,i):
+    llist= os.listdir(background)
+    lweights = create_weights(llist)
+    output_path = 'output/output'+str(i)+'.mp4'
+    bg_i = pick1(llist,lweights)
+    
+
+    (
+    ffmpeg 
+    .filter([ffmpeg.input(background + bg_i[0]),ffmpeg.input(myPNG)],'overlay',10,10)
+    .output(output_path)
+    .run()
+    )
+
+    
